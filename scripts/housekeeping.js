c925e2b6766d286cc73161c186b9176d1fae1f8a
bluepropane <rbluepropane@gmail.com>
2019-11-27 23:47:52 +0800

    build(housekeeping.js): add script to update common package.json fields across all scopes
    And at the same time execute the script

---
+++ scripts/housekeeping.js
@@ -0,0 +1,25 @@
+#!/usr/bin/env node
+const path = require('path');
+const fs = require('fs');
+
+const pkgPath = path.join(process.cwd(), 'package.json');
+const pkg = require(pkgPath);
+console.log(pkg.name);
+
+const commonFields = {
+  repository: 'https://github.io/bluepropane/chromex',
+  author: 'bluepropane <rbluepropane@gmail.com>',
+  license: 'MIT',
+  keywords: [
+    'chrome-extension',
+    'chrome',
+    'boilerplate',
+    'livereload',
+    'live-reload',
+    'extension',
+  ],
+};
+
+Object.assign(pkg, commonFields);
+
+fs.writeFile(pkgPath, JSON.stringify(pkg, null, 2), console.log);
